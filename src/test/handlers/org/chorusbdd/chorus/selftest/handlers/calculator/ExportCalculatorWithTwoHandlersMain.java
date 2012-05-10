ae144fad106481fe617dbeb63149b0e4bcd24028
Nick Ebbutt <nick@objectdefinitions.com>
2012-05-10 17:57:11 +0100

    Rearranging junit and handler classes into separate test/src directories

---
+++ src/test/handlers/org/chorusbdd/chorus/selftest/handlers/calculator/ExportCalculatorWithTwoHandlersMain.java
@@ -0,0 +1,33 @@
+package org.chorusbdd.chorus.selftest.handlers.calculator;
+
+import org.chorusbdd.chorus.annotations.Handler;
+import org.chorusbdd.chorus.annotations.Step;
+import org.chorusbdd.chorus.remoting.jmx.ChorusHandlerJmxExporter;
+
+/**
+ * Used to run a simple process that can be used to test Chorus features
+ *
+ * Created by: Steve Neal
+ * Date: 14/11/11
+ */
+public class ExportCalculatorWithTwoHandlersMain {
+
+    public static void main(String[] args) throws Exception {
+        //export a calculator handlers
+
+        new ChorusHandlerJmxExporter(new EchoingHandler());
+
+        ChorusHandlerJmxExporter exporter = new ChorusHandlerJmxExporter(new CalculatorHandler());
+        exporter.getStepMetadata();
+
+        Thread.sleep(1000 * 60 * 5); //keeps process alive for 5 mins
+    }
+
+    @Handler("Echo Values")
+    public static class EchoingHandler {
+        @Step("^.*accept a string with value '(.*)'$")
+        public void accept(String s) {
+            System.out.println(s);
+        }
+    }
+}
