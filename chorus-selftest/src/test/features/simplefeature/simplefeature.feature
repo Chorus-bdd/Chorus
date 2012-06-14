d171ce579d5ea55faff155fe624918627e55956a
Nick Ebbutt <nick@objectdefinitions.com>
2012-06-14 15:15:58 +0100

    Add selftest module

---
+++ chorus-selftest/src/test/features/simplefeature/simplefeature.feature
@@ -0,0 +1,8 @@
+Feature: Simple Feature
+
+  Test that Chorus can run a very simple feature, with a single Scenario
+
+  Scenario: Simple Scenario
+    Given Chorus is working properly
+    I can run a feature with a single scenario
+
---
+++ chorus-selftest/src/test/handlers/simplefeature/SimpleFeatureHandler.java
@@ -0,0 +1,16 @@
+package simplefeature;
+
+import org.chorusbdd.chorus.annotations.Handler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Nick Ebbutt
+ * Date: 14/06/12
+ * Time: 09:21
+ */
+@Handler("Simple Feature")
+public class SimpleFeatureHandler {
+
+
+
+}
