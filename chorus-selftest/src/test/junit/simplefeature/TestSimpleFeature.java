801e20160f19a8bcc371fff05ef028369200501d
Nick Ebbutt <nick@objectdefinitions.com>
2012-06-25 23:43:56 +0100

    Correct interpreter exit code

---
+++ chorus-selftest/src/test/junit/simplefeature/TestSimpleFeature.java
@@ -0,0 +1,11 @@
+package simplefeature;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nick
+ * Date: 25/06/12
+ * Time: 22:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestSimpleFeature {
+}
