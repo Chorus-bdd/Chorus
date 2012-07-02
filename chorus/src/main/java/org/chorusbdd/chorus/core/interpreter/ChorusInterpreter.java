a238cc951397c6915473ce7fea2d7c87b22a638c
Nick Ebbutt <nick@objectdefinitions.com>
2012-07-02 13:35:52 +0100

    Add / Update license headers

--- chorus/src/main/java/org/chorusbdd/chorus/core/interpreter/ChorusInterpreter.java
+++ chorus/src/main/java/org/chorusbdd/chorus/core/interpreter/ChorusInterpreter.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright (C) 2000-2012 The Software Conservancy as Trustee.
+ *  Copyright (C) 2000-2012 The Software Conservancy and Original Authors.
  *  All rights reserved.
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -27,7 +27,6 @@
  *  the Software, or for combinations of the Software with other software or
  *  hardware.
  */
-
 package org.chorusbdd.chorus.core.interpreter;
 
 import org.chorusbdd.chorus.annotations.*;
--- chorus/src/main/java/org/chorusbdd/chorus/core/interpreter/results/ScenarioToken.java
+++ chorus/src/main/java/org/chorusbdd/chorus/core/interpreter/results/ScenarioToken.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright (C) 2000-2012 The Software Conservancy as Trustee.
+ *  Copyright (C) 2000-2012 The Software Conservancy and Original Authors.
  *  All rights reserved.
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
