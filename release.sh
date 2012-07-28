d1d57d1b3370a49c3307fd1065e066aacdb06866
Nick Ebbutt <nick@objectdefinitions.com>
2012-07-28 22:39:13 +0100

    Create tar.gz and zipfile in release.sh

--- release.sh
+++ release.sh
@@ -13,6 +13,14 @@ function fn_promptForContinue {
   fi
 }
 
+read VERSION?"What version are we releasing?"
+echo $VERSION
+fn_promptForContinue
+
+
+
+echo "Generating release script.."
+
 function fn_genReleaseScript {
   echo "Generating release script for $1"
 
@@ -27,17 +35,49 @@ function fn_genReleaseScript {
     echo ${COMMAND} >> releaseScript.sh
 }
 
-read VERSION?"What version are we releasing?"
-echo $VERSION
-fn_promptForContinue
-
 if [ -f ./releaseScript.sh ] ; then
   rm ./releaseScript.sh
 fi
-
 fn_genReleaseScript chorus
 fn_genReleaseScript chorus-spring
 fn_genReleaseScript chorus-tools
+echo "Generated release script in ./releaseScript.sh"
+
+
+echo "Now creating tar.gz and zip for release.."
+function fn_addToTar {
+    tar -rf ${TARFILE} $1
+    zip ${ZIPFILE} $1
+}
+
+function fn_addArtifactsToTar {
+    basedir=`pwd`
+    cd ${basedir}/$1/target
+    fn_addToTar $1-${VERSION}.jar
+    fn_addToTar $1-${VERSION}-javadoc.jar
+    fn_addToTar $1-${VERSION}-sources.jar
+    cd ${basedir}
+}
+
+TARFILE=`pwd`/chorus-${VERSION}.tar
+ZIPFILE=`pwd`/chorus-${VERSION}.zip
+if [ -f "${TARFILE}" ] ; then
+    rm ${TARFILE}
+fi
+if [ -f "${TARFILE}.gz" ] ; then
+    rm ${TARFILE}.gz
+fi
+if [ -f "${ZIPFILE}" ] ; then
+    rm ${ZIPFILE}
+fi
+
+fn_addArtifactsToTar chorus
+fn_addArtifactsToTar chorus-spring
+fn_addArtifactsToTar chorus-tools
+fn_addToTar ./changelist.xml
+fn_addToTar ./changelist.xsl
+gzip ${TARFILE}
+
 
 
 
