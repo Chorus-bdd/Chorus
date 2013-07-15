61fa58dc350e89bbb0cfb63960792b170e369063
Nick Ebbutt <nick@objectdefinitions.com>
2013-07-15 23:15:25 +0100

    fix failing test

---
+++ chorus/src/test/junit/org/chorusbdd/chorus/dev/Hello.java
@@ -0,0 +1,15 @@
+package org.chorusbdd.chorus.dev;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nick
+ * Date: 15/07/13
+ * Time: 23:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hello {
+    
+    public static void main(String[] arg) {
+        System.out.println("Hello");
+    }
+}
