87fa2daa35b37d62653e4edb3f589440a7ef0562
Nick Ebbutt <nick@objectdefinitions.com>
2013-11-06 18:50:58 +0000

    Update file headers

--- chorus-selftest/src/test/features/org/chorusbdd/chorus/selftest/featureparsing/simplefeature/TestFeatureParsing.java
+++ chorus-selftest/src/test/features/org/chorusbdd/chorus/selftest/featureparsing/simplefeature/TestFeatureParsing.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright (C) 2000-2012 The Software Conservancy and Original Authors.
+ *  Copyright (C) 2000-2013 The Software Conservancy and Original Authors.
  *  All rights reserved.
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
