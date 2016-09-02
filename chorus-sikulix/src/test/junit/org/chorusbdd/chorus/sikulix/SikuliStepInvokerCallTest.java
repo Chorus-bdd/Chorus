01a6d96f5a17ed3fe9e054c37bcd5b0b39e27195
Nick Ebbutt <nick@ebbutt.net>
2016-09-02 19:19:45 +0100

    Skip Sikuli test which on Linux which is not compatible with the OS at present

--- chorus-sikulix/src/test/junit/org/chorusbdd/chorus/sikulix/SikuliStepInvokerCallTest.java
+++ chorus-sikulix/src/test/junit/org/chorusbdd/chorus/sikulix/SikuliStepInvokerCallTest.java
@@ -3,6 +3,7 @@ package org.chorusbdd.chorus.sikulix;
 import org.chorusbdd.chorus.logging.ChorusLog;
 import org.chorusbdd.chorus.logging.ChorusLogFactory;
 import org.chorusbdd.chorus.stepinvoker.StepInvoker;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.nio.file.Path;
@@ -12,6 +13,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.junit.Assume.assumeTrue;
 
 /**
  * TODO comments???
@@ -20,57 +22,68 @@ import static org.junit.Assert.*;
  */
 public class SikuliStepInvokerCallTest {
 
-	private ChorusLog log = ChorusLogFactory.getLog(this.getClass());
+    private ChorusLog log = ChorusLogFactory.getLog(this.getClass());
 
-	private final Path sikuliRoot = Paths.get("src", "test", "sikulis");
+    private final Path sikuliRoot = Paths.get("src", "test", "sikulis");
 
-	@Test
-	public void canReturnValueFromStepInvoker() throws ReflectiveOperationException {
-		SikuliManager sikuliManager = new SikuliManager(sikuliRoot);
-		List<StepInvoker> stepInvokers = sikuliManager.getStepInvokers();
+    @Before
+    public void skipIfNixOS() {
+        //This test will not run on Linux at present
+        //[error] RunTimeAPI: loadLib: libVisionProxy.so not usable
+        assumeTrue(! isUnix());
+    }
 
-		StepInvoker stepInvoker = findById(stepInvokers, "status.getIntValue");
-		assertNotNull(stepInvoker);
-		Object returnValue = stepInvoker.invoke(Collections.<String>emptyList());
+    @Test
+    public void canReturnValueFromStepInvoker() throws ReflectiveOperationException {
+        SikuliManager sikuliManager = new SikuliManager(sikuliRoot);
+        List<StepInvoker> stepInvokers = sikuliManager.getStepInvokers();
 
-		assertEquals(123, returnValue);
-	}
+        StepInvoker stepInvoker = findById(stepInvokers, "status.getIntValue");
+        assertNotNull(stepInvoker);
+        Object returnValue = stepInvoker.invoke(Collections.<String>emptyList());
 
-	@Test
-	public void canPassValueToStepInvoker() throws ReflectiveOperationException {
-		SikuliManager sikuliManager = new SikuliManager(sikuliRoot);
-		List<StepInvoker> stepInvokers = sikuliManager.getStepInvokers();
+        assertEquals(123, returnValue);
+    }
 
-		StepInvoker stepInvoker = findById(stepInvokers, "status.sumValues");
-		assertNotNull(stepInvoker);
-		Object returnValue = stepInvoker.invoke(Arrays.asList("Steve", "Lake"));
+    @Test
+    public void canPassValueToStepInvoker() throws ReflectiveOperationException {
+        SikuliManager sikuliManager = new SikuliManager(sikuliRoot);
+        List<StepInvoker> stepInvokers = sikuliManager.getStepInvokers();
 
-		assertEquals("SteveLake", returnValue);
-	}
+        StepInvoker stepInvoker = findById(stepInvokers, "status.sumValues");
+        assertNotNull(stepInvoker);
+        Object returnValue = stepInvoker.invoke(Arrays.asList("Steve", "Lake"));
 
-	@Test
-	public void canRetrieveException() throws ReflectiveOperationException {
-		SikuliManager sikuliManager = new SikuliManager(sikuliRoot);
-		List<StepInvoker> stepInvokers = sikuliManager.getStepInvokers();
+        assertEquals("SteveLake", returnValue);
+    }
 
-		StepInvoker stepInvoker = findById(stepInvokers, "status.badCall");
-		assertNotNull(stepInvoker);
-		try {
-			Object returnValue = stepInvoker.invoke(Arrays.asList("Steve", "Lake"));
-			fail();
-		}
-		catch(RuntimeException re) {
-			assertTrue(re.getMessage().contains("line 17, in badCall"));
-		}
-	}
+    @Test
+    public void canRetrieveException() throws ReflectiveOperationException {
+        SikuliManager sikuliManager = new SikuliManager(sikuliRoot);
+        List<StepInvoker> stepInvokers = sikuliManager.getStepInvokers();
 
-	private StepInvoker findById(List<StepInvoker> stepInvokers, String id) {
-		for (StepInvoker stepInvoker : stepInvokers) {
-			if (stepInvoker.getId().equals(id)) {
-				return stepInvoker;
-			}
-		}
-		return null;
-	}
+        StepInvoker stepInvoker = findById(stepInvokers, "status.badCall");
+        assertNotNull(stepInvoker);
+        try {
+            Object returnValue = stepInvoker.invoke(Arrays.asList("Steve", "Lake"));
+            fail();
+        }
+        catch(RuntimeException re) {
+            assertTrue(re.getMessage().contains("line 17, in badCall"));
+        }
+    }
 
+    private StepInvoker findById(List<StepInvoker> stepInvokers, String id) {
+        for (StepInvoker stepInvoker : stepInvokers) {
+            if (stepInvoker.getId().equals(id)) {
+                return stepInvoker;
+            }
+        }
+        return null;
+    }
+
+    public boolean isUnix() {
+        String os = System.getProperty("os.name").toLowerCase();
+        return (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0 || os.indexOf("aix") > 0 );
+    }
 }
