41be711dc09b0f0be8aa24064d0f62123ad80c52
Nick Ebbutt <nick@ebbutt.net>
2016-12-26 14:21:47 +0000

    Improve chorus.sh distribution script

--- src/main/dist/chorus.sh
+++ src/main/dist/chorus.sh
@@ -7,7 +7,21 @@ if [ -z "${VERSION}" ] ; then
   exit 1
 fi
 
-java -cp './lib/*' org.chorusbdd.chorus.Chorus $* -console
+ARGS=$*
+
+#If running standalone there's probably no need to specify custom handler class packages
+#but specifying a value for this switch is presently mandatory
+if [[ "${ARGS}" != *"-h"* -a "${ARGS}" != *"-handlerPackages"* ]] ; then
+  ARGS="${ARGS} -h no.user.handler.packages"
+fi
+
+if [[ "${ARGS}" != *"-c"* -a "${ARGS}" != *"-console"* ]] ; then
+  ARGS="${ARGS} -console"
+fi
+
+java -cp './lib/*' org.chorusbdd.chorus.Chorus ${ARGS}
+
+
 
 
 
