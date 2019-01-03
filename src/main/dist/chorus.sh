d0557ad7b8513cdfbf54d785d7e65fa75bf1a6bd
Nick Ebbutt <nick@ebbutt.net>
2019-01-03 09:12:50 +0100

    Remove Java version check and the -h handler package switch is no longer mandatory

--- src/main/dist/chorus.sh
+++ src/main/dist/chorus.sh
@@ -1,27 +1,15 @@
 #!/usr/bin/env bash
 
-VERSION=`java -version 2>&1 | grep '1.8'`
-
-if [ -z "${VERSION}" ] ; then
-  echo "You don't have java version 1.8 in your PATH"
-  exit 1
-fi
-
 ARGS=$*
 
-#If running standalone there's probably no need to specify custom handler class packages
-#but specifying a value for this switch is presently mandatory
-if [[ "${ARGS}" != *"-h"* ]] ; then
-  ARGS="${ARGS} -h no.user.handler.packages"
-fi
-
-#Detect if user passed a console arg, so that passing '-c false' , or '-console false' can be used to turn off console mode, even if running within a terminal
+# Detect if user passed a console arg, so that passing '-c false' , or '-console false' 
+# can be used to turn off console mode, even if running within a terminal
 NO_CONSOLE_IN_ARGS=true
 if [[ "${ARGS}" == *"-c"* ]] ; then
   NO_CONSOLE_IN_ARGS=false
 fi
 
-# Find the directory containg the chorus script being called
+# Find the directory containing the chorus script being called
 # We need this to set the classpath to contain the libs
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
